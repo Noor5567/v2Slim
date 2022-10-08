1d382beab631b856a6191fe70c7bba1e82cea048
Noor5567 <83994879+Noor5567@users.noreply.github.com>
2022-10-08 15:34:22 +0300

    Update datatable.php

--- datatable.php
+++ datatable.php
@@ -5,6 +5,7 @@
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.jqueryui.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/scroller/2.0.7/css/scroller.jqueryui.min.css">
+    <link rel="stylesheet" href="//cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
 </head>
 
 <body>
@@ -24,65 +25,76 @@
     <script src="https://cdn.datatables.net/scroller/2.0.7/js/dataTables.scroller.min.js"></script>
 
     <script>
-    $(document).ready(function() {
-        // $('#example').DataTable({
-        //     ajax: "apiManager/getSabyTable",
-        //     deferRender: true,
-        //     scrollY: 200,
-        //     scrollCollapse: true,
-        //     scroller: true,
-        //     // success: function(response) {
-        //     //     var fullString = '';
-        //     //     var jsonArray = JSON.parse(response);
-        //     //     jsonArray.forEach(function(jsonitem) {
-        //     //         fullString += "<tr>";
-        //     //         fullString += "<td>" + jsonItem.id + "</td>";
-        //     //         fullString += "<td>" + jsonItem.saby + "</td>";
-        //     //         fullString += "<td>" + jsonItem.server + "</td></tr>";
-
-
-        //     //     });
-        //     //     console.log(fullString);
-        //     //     $('#example tbody').append(fullString);
-        //     //     $('#example').draw();
-        //     // }
-        // });
-
         $(document).ready(function() {
-            $('#example').DataTable().destroy();
-            $('#example').DataTable({
+            // $('#example').DataTable({
+            //     ajax: "apiManager/getSabyTable",
+            //     deferRender: true,
+            //     scrollY: 200,
+            //     scrollCollapse: true,
+            //     scroller: true,
+            //     // success: function(response) {
+            //     //     var fullString = '';
+            //     //     var jsonArray = JSON.parse(response);
+            //     //     jsonArray.forEach(function(jsonitem) {
+            //     //         fullString += "<tr>";
+            //     //         fullString += "<td>" + jsonItem.id + "</td>";
+            //     //         fullString += "<td>" + jsonItem.saby + "</td>";
+            //     //         fullString += "<td>" + jsonItem.server + "</td></tr>";
 
-                //destroy: true,
-                "ajax": {
-                    "url": "apiManager/getSabyTable", //task do refresh the table after 30 sec 
-                },
-                "columns": [{
-                        "data": "id"
-                    },
-                    {
-                        "data": "saby"
-                    },
-                    {
-                        "data": "server"
+
+            //     //     });
+            //     //     console.log(fullString);
+            //     //     $('#example tbody').append(fullString);
+            //     //     $('#example').draw();
+            //     // }
+            // });
+            function getSabyTable() {
+                var table = $('#example').DataTable();
+                table.destroy();
+                $.ajax({
+                    type: "GET",
+                    url: "apiManager/getSabyTable",
+                    success: function(response) {
+                        $('#example').DataTable({
+                            //destroy: true,
+                            "ajax": {
+                                "url": "apiManager/getSabyTable", //task do refresh the table after 30 sec 
+                            },
+                            "oLanguage": {
+                                "oPaginate": {
+                                    "sPrevious": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-left"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>',
+                                    "sNext": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>'
+                                },
+                                "sInfo": "Showing page _PAGE_ of _PAGES_",
+                                "sSearch": '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>',
+                                "sSearchPlaceholder": "Search...",
+                                "sLengthMenu": "Results :  _MENU_",
+                            },
+                            dom: '<"container-fluid"<"row"<"col"B><"col"l><"col"f>>>rtip',
+                            "lengthMenu": [100, 500, 1000],
+                            "pageLength": 100,
+                            "columns": [{
+                                    "data": "id"
+                                },
+                                {
+                                    "data": "saby"
+                                },
+                                {
+                                    "data": "server"
+                                }
+                            ]
+                        });
                     }
-                ]
-
+                });
+            }
+            $(document).ready(function() {
+                getSabyTable();
+                $('#example').DataTable().destroy();
+                setInterval(function() {
+                    getSabyTable();
+                }, 10000);
             });
-            /*
-            setInterval(function() {
-                table.ajax.reload();
-            }, 10000);*/
-
-            setInterval(function() {
-                $('#example').load("apiManager/getSabyTable");
-                refresh();
-            }, 10000);
-            //table.rows.add(columns).draw()
-            //$('#example').draw(result);
-
-            //setInterval('#example', 30000);
-        });
-    })
+        })
     </script>
 </body>
 
